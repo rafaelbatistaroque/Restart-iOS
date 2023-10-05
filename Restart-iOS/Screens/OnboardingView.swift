193ba98da576e01693a1413d005b5ed8a4972309
Rafael Batista <maranata_rafa@hotmail.com>
2023-10-05 02:43:09 +0100

    FEAT: created footer section

--- Restart-iOS/Screens/OnboardingView.swift
+++ Restart-iOS/Screens/OnboardingView.swift
@@ -51,6 +51,57 @@ struct OnboardingView: View {
 
                 Spacer()
                 //MARK: - FOOTER
+
+                ZStack{
+                    //1. BACKGROUND (STATIC)
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                        .padding(8)
+                    
+                    //2. CALL-TO-ACTION (STATIC)
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundStyle(.white)
+                        .offset(x: 20)
+
+                    //3. CAPSULE (DYNAMIC WIDTH)
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        
+                        Spacer()
+                    }
+
+                    //4. CIRCLE (DRAGGABLE)
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .black))
+                        }
+                        .foregroundStyle(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .onTapGesture {
+                            isOnboardingViewActive = false
+                        }
+
+                        Spacer()
+                    }
+
+                }
+                .frame(height: 80, alignment: .center)
+                .padding()
             }
         }
     }
